be08393115d9bed5159a59d26303bde098894b9e
ingjfnf <161249718+ingjfnf@users.noreply.github.com>
2024-07-10 17:21:42 -0500

    Update dash.py

--- dash.py
+++ dash.py
@@ -768,15 +768,8 @@ if st.session_state.show_dataframe:
 
         st.plotly_chart(fig)
 
-        def descargar_excel_d(dataframe):
-            output = io.BytesIO()
-            with pd.ExcelWriter(output, engine='openpyxl') as writer:
-                dataframe.to_excel(writer, index=False, sheet_name='Sheet1')
-            processed_data = output.getvalue()
-            return processed_data
 
-
-        excel_data = descargar_excel_d(df_distribucion)
+        excel_data = descargar_excel(df_distribucion)
         st.download_button(
             label="Descargar Datos",
             data=excel_data,
